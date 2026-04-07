06c8598aa9ed562142fb9cb20d3998f770117b8d
sinha ji-dev <redxfury5@gmail.com>
2026-04-07 20:04:09 +0530

    Fetch and display reviews from API

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-// document.querySelectorAll(".outline-btn").forEach(btn => {
-//   btn.addEventListener("click", () => {
-//     alert("Renew request sent!");
-//   });
-// });
+fetch("http://127.0.0.1:5000/reviews")
+.then(res => res.json())
+.then(data => {
+    const container = document.getElementById("reviews-container");
+
+    data.forEach(review => {
+        const div = document.createElement("div");
+        div.classList.add("review-card");
+
+        div.innerHTML = `
+            <h4>${review.name}</h4>
+            <p>${review.review}</p>
+            <p>⭐ ${review.rating}</p>
+        `;
+
+        container.appendChild(div);
+    });
+})
+.catch(err => console.error(err));
